dcfae4368a26d4071288ca9eb92f1fafbf5ab57c
Écio Ferraz <ecioferraz@hotmail.com>
2021-12-20 14:14:39 -0300

    Updates

---
+++ precache-manifest.4c099195800c4ee6d7826d87cbe5675e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35606f048ceae2115ce6758acffda6ff",
+    "url": "/trybe-trivia-project/index.html"
+  },
+  {
+    "revision": "f6af44f65b95894e6063",
+    "url": "/trybe-trivia-project/static/css/main.e24d566e.chunk.css"
+  },
+  {
+    "revision": "2bfe6fe130e22c8015e1",
+    "url": "/trybe-trivia-project/static/js/2.acca1d9d.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/trybe-trivia-project/static/js/2.acca1d9d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f6af44f65b95894e6063",
+    "url": "/trybe-trivia-project/static/js/main.98387c85.chunk.js"
+  },
+  {
+    "revision": "31055c9c88c3f5ea751f",
+    "url": "/trybe-trivia-project/static/js/runtime-main.dc4fa4a5.js"
+  },
+  {
+    "revision": "98ce48ec378fbe95c6a0caf5b3437bbe",
+    "url": "/trybe-trivia-project/static/media/game-background.98ce48ec.jpg"
+  },
+  {
+    "revision": "c6f26eac59f81716e9e3af9149cfeeab",
+    "url": "/trybe-trivia-project/static/media/login-background.c6f26eac.jpg"
+  },
+  {
+    "revision": "466d153e266e6e813c5ef2c55aa5fc2c",
+    "url": "/trybe-trivia-project/static/media/trivia.466d153e.png"
+  }
+]);
